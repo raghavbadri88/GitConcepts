3f00cb2b02de362565720767c9c71ee0737e30ba
raghavbadri88 <raghavbadri88@gmail.com>
2020-09-16 22:12:49 +0530

    ClassC Added

---
+++ src/com/git/org/ClassC.java
@@ -0,0 +1,18 @@
+package com.git.org;
+
+public class ClassC {
+	public void meth() {
+		System.out.println("Im in Master");
+
+	}
+	
+	public void meth1() {
+		System.out.println("Master A1");
+
+	}
+	
+	public void meth2() {
+		System.out.println("Master A2");
+
+	}
+}
